0c139dac99ab17e08a24ccae7a7799225f931300
muhammadfauzanis <mfauzann982@gmail.com>
2025-06-19 10:11:37 +0700

    change port

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-ENV PORT=8000
+ENV PORT=8080
 
 CMD gunicorn app.main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
--- run.py
+++ run.py
@@ -2,5 +2,5 @@ import uvicorn
 import os
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000)) 
+    port = int(os.getenv("PORT", 8080)) 
     uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=True)
